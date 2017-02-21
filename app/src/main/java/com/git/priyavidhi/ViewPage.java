30302bc181165237fa0dffac7fd4a6b271875bcf
Vidhi <vidhi.bhut@inexture.in>
2017-02-21 14:49:20 +0530

    backBtn

--- app/src/main/java/com/git/priyavidhi/ViewPage.java
+++ app/src/main/java/com/git/priyavidhi/ViewPage.java
@@ -2,10 +2,21 @@ package com.git.priyavidhi;
 
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 public class ViewPage extends AppCompatActivity {
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+
+        overridePendingTransition(
+                R.anim.anim_slide_in_right,
+                R.anim.anim_slide_out_right);
+    }
 
     ViewPager viewPager;
     PagerAdapter adapter;
@@ -14,6 +25,18 @@ public class ViewPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_page);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        android.support.v7.widget.Toolbar toolbar1 = (android.support.v7.widget.Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar1);
+
+
+        ActionBar ab = getSupportActionBar();
+        ab.setHomeAsUpIndicator(R.drawable.ic_action_navigation_arrow_back);
+        ab.setDisplayHomeAsUpEnabled(true);
+//        ab.setTitle("Ratri B4 Navratri");//get it dynamiccally
 
         int v1=getIntent().getIntExtra("pos",0);
 
@@ -26,5 +49,17 @@ public class ViewPage extends AppCompatActivity {
         // Binds the Adapter to the ViewPager
         viewPager.setAdapter(adapter);
         viewPager.setCurrentItem(v1);
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item != null && item.getItemId() == android.R.id.home) {
+            finish();
+
+            overridePendingTransition(
+                    R.anim.anim_slide_in_right,
+                    R.anim.anim_slide_out_right);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
